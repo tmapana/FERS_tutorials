21c66a126a42cf7d6f072190eef15657bcc0b42c
tmapana <tlotlisomapana@gmail.com>
2022-06-08 15:45:44 +0200

    Update to notebook

--- fers.py
+++ fers.py
@@ -220,6 +220,7 @@ class FersXMLGenerator:
 
         for i in range (0, int(np.size(x))):
             add_point(path, x[i], y[i], z[i], t[i])
+            print(y[i])
 
         target = SubElement(platform, 'target')
         target.set('name', name)
