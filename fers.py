34e0e774bcbf4661ba50ef2ba8234025fc75c4f4
tmapana <tlotlisomapana@gmail.com>
2022-08-19 17:29:13 +0200

    Errors in producing SAR image under investigation.

--- fers.py
+++ fers.py
@@ -45,6 +45,7 @@ def read_hdf5(filename):
     # for attr in attribute_list:
         # print(attr, h5[dataset_list[0]].attrs[attr])
 
+    # scale = np.float64(h5[dataset_list[0]].attrs['fullscale'])
     scale = np.float64(h5[dataset_list[0]].attrs['fullscale'])
     # rate = np.float64(h5[dataset_list[0]].attrs['rate'])
     # time = np.float64(h5[dataset_list[0]].attrs['time'])
@@ -52,12 +53,12 @@ def read_hdf5(filename):
     n_pulses = int(np.floor(np.size(dataset_list)/2))
     ns_pulse = int(np.size(h5[dataset_list[0]]))
 
-    i_matrix = np.zeros((n_pulses, ns_pulse), dtype='float64')
-    q_matrix = np.zeros((n_pulses, ns_pulse), dtype='float64')
+    i_matrix = np.zeros((n_pulses, ns_pulse), dtype='float64')  #changed = float64
+    q_matrix = np.zeros((n_pulses, ns_pulse), dtype='float64')  #changed
 
     for i in range(0, n_pulses):
-        i_matrix[i, :] = np.array(h5[dataset_list[2*i + 0]], dtype='float64')
-        q_matrix[i, :] = np.array(h5[dataset_list[2*i + 1]], dtype='float64')
+        i_matrix[i, :] = np.array(h5[dataset_list[2*i + 0]], dtype='float64')   #changed
+        q_matrix[i, :] = np.array(h5[dataset_list[2*i + 1]], dtype='float64')   #changed
 
     dataset = np.array(i_matrix + 1j*q_matrix).astype('complex128')
 
@@ -99,6 +100,9 @@ class FersXMLGenerator:
 
         light_speed = SubElement(parameters, 'c')
         light_speed.text = str(const.speed_of_light)
+
+        export = SubElement(parameters, 'export csv="false" binary="true" csvbinary="false"')
+        export.text = str()
 
     def add_pulse(self, name, pulse_file, power_watts, centre_freq):
         pulse = SubElement(self.simulation, 'pulse')
---
+++ out/1m_30ms.cmd
@@ -0,0 +1,37 @@
+miloSAR azimuth compression command file (azcom)
+$ProgramVersion (jmh)         => 1.1
+
+$ScreenUpdateRate             => 1
+$LogFileName                  => /home/tlotliso/Documents/FERS/FERS_tutorials/out/1m_30ms.log
+$InputStartSampleDelay        => 0
+$CarrierFreq [Hz]             => 2450000.0
+$InputPRF [Hz]                => 1250
+$NomGroundSpeed [m/s]         => 30
+$InputFileAzPts               => 4096
+$StartProcessAzPt             => 0
+$AzPtsToProcess               => 4096
+$InputFileRngBins             => 1250
+$StartProcessRngBin           => 0
+$RngBinsToProcess             => 1250
+$InputDCOffsetI               => 0.0
+$InputDCOffsetQ               => 0.0
+$InvFFTSizeReduc [pow of 2]   => 1
+$InputFileName                => /home/tlotliso/Documents/FERS/FERS_tutorials/out/1m_30ms.rng
+$OutputFileName               => /home/tlotliso/Documents/FERS/FERS_tutorials/out/1m_30ms.azi
+$AppendExistOutFileFlg [Y/N]  => N
+$RngFocSegments               => -1
+$RefFuncSign [+-1]            => 1
+$A2DFreq [Hz]                 => 4095999.9999999995
+$NomAzRes [m]                 => 0.54
+$WinConstTime [0.0-1.0]       => 0.08
+$NumLooks                     => 1
+$LookOverlapFrac [0.0-1.0]    => 0.0
+$WinConstFreq [0.0-1.0]       => 0.08
+$RngCurvInterpSize            => 4
+$RngCurvBatchSize             => 16
+$PostSumRatio                 => 1
+$DetectMethod                 => 0
+$InputDataType                => 3
+$OutputDataType               => 3
+$Scale                        => 1
+$ReportMax [1/0]              => 1
