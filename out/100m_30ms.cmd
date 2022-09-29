039d9ba043752347d66dea51ed0df72312d57d5f
tmapana <tlotlisomapana@gmail.com>
2022-09-29 16:24:48 +0200

    Made changes to data format to suit G2 requirements.

---
+++ out/100m_30ms.cmd
@@ -0,0 +1,37 @@
+miloSAR azimuth compression command file (azcom)
+$ProgramVersion (jmh)         => 1.1
+
+$ScreenUpdateRate             => 1
+$LogFileName                  => /home/tlotliso/Documents/FERS/FERS_tutorials/out/100m_30ms.log
+$InputStartSampleDelay        => 0
+$CarrierFreq [Hz]             => 2450000000.0
+$InputPRF [Hz]                => 1250
+$NomGroundSpeed [m/s]         => 30
+$InputFileAzPts               => 1250
+$StartProcessAzPt             => 0
+$AzPtsToProcess               => 1250
+$InputFileRngBins             => 2047
+$StartProcessRngBin           => 0
+$RngBinsToProcess             => 2047
+$InputDCOffsetI               => 0.0
+$InputDCOffsetQ               => 0.0
+$InvFFTSizeReduc [pow of 2]   => 1
+$InputFileName                => /home/tlotliso/Documents/FERS/FERS_tutorials/out/100m_30ms.rng
+$OutputFileName               => /home/tlotliso/Documents/FERS/FERS_tutorials/out/100m_30ms.azi
+$AppendExistOutFileFlg [Y/N]  => N
+$RngFocSegments               => -1
+$RefFuncSign [+-1]            => 1
+$A2DFreq [Hz]                 => 163840000.0
+$NomAzRes [m]                 => 3.134419048943256
+$WinConstTime [0.0-1.0]       => 0.08
+$NumLooks                     => 1
+$LookOverlapFrac [0.0-1.0]    => 0.0
+$WinConstFreq [0.0-1.0]       => 0.08
+$RngCurvInterpSize            => 16
+$RngCurvBatchSize             => 64
+$PostSumRatio                 => 1
+$DetectMethod                 => 0
+$InputDataType                => 3
+$OutputDataType               => 3
+$Scale                        => 1
+$ReportMax [1/0]              => 1
